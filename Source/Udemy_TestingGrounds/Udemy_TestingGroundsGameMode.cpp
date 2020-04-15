528f29a335b25d5ebf8fe805af6196cb72b79782
Archoney <kocjan86@gmail.com>
2020-04-15 10:52:58 +0200

    Refactoring

--- Source/Udemy_TestingGrounds/Udemy_TestingGroundsGameMode.cpp
+++ Source/Udemy_TestingGrounds/Udemy_TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AUdemy_TestingGroundsGameMode::AUdemy_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Character/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
